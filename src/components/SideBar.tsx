7f6149a91d408c4361eb16525c5a900d8411427b
Jean Morillo Sanchez <jemorillosanchez@redventures.com>
2023-03-03 01:03:35 -0400

    restructures sidebar and links

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement } from "react";
 import Image from "next/image";
 import Icons from "@/styles/icons/Icons";
+import Link from "next/link";
 export default function SideBar({ children }: { children: ReactElement }) {
   return (
     <>
@@ -61,8 +62,8 @@ export default function SideBar({ children }: { children: ReactElement }) {
           </a>
           <ul className="space-y-2">
             <li>
-              <a
-                href="#"
+              <Link
+                href="/"
                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 <svg
@@ -84,11 +85,11 @@ export default function SideBar({ children }: { children: ReactElement }) {
                 <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
                   Pro
                 </span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                href="/"
                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 <svg
@@ -101,12 +102,13 @@ export default function SideBar({ children }: { children: ReactElement }) {
                   <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
                   <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
                 </svg>
-                <span className="ml-3">Dashboard</span>
-              </a>
+                <span className="ml-3 line-through">Dashboard</span>
+              </Link>
             </li>
             <li>
               <a
-                href="#"
+                href="https://www.linkedin.com/in/jean-morillo-30/"
+                target="_blank"
                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 <svg
@@ -118,7 +120,13 @@ export default function SideBar({ children }: { children: ReactElement }) {
                 >
                   <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Kanban</span>
+                <span className="flex-1 ml-3 whitespace-nowrap line-through flex items-center">
+                  Experiences{" "}
+                  <Icons
+                    type="up-right-arrow"
+                    className="fill-black h-[12px] ml-2"
+                  />
+                </span>
                 <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
                   Pro
                 </span>
@@ -126,7 +134,7 @@ export default function SideBar({ children }: { children: ReactElement }) {
             </li>
             <li>
               <a
-                href="#"
+                href="mailto:morillo30@gmail.com"
                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 <svg
@@ -149,7 +157,8 @@ export default function SideBar({ children }: { children: ReactElement }) {
             </li>
             <li>
               <a
-                href="#"
+                href="https://www.amazon.com/Razer-Blade-16-Gaming-Laptop/dp/B0BSRJ3SQ3/ref=sr_1_5?crid=26ZSZHSG0D45A&keywords=razer+blade&qid=1677817800&s=electronics&sprefix=razer+blad%2Celectronics%2C123&sr=1-5&ufe=app_do%3Aamzn1.fos.17f26c18-b61b-4ce9-8a28-de351f41cffb"
+                target="_blank"
                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 <svg
@@ -168,48 +177,7 @@ export default function SideBar({ children }: { children: ReactElement }) {
                 <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
               </a>
             </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
-              </a>
-            </li>
+
             <li>
               <a
                 href="#"
