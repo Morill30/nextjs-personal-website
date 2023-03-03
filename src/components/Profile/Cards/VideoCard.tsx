e91b23aa23d3ea5b3a2380fc39f7bcd29ebf04e3
Jean Morillo Sanchez <jemorillosanchez@redventures.com>
2023-03-03 01:05:07 -0400

    Transform everithing as part of a single video component

--- src/components/Profile/Cards/VideoCard.tsx
+++ src/components/Profile/Cards/VideoCard.tsx
@@ -1,18 +1,30 @@
 import Icons from "@/styles/icons/Icons";
-import type { ReactElement } from "react";
+import Image from "next/image";
 
 export default function VideoCard({
-  children,
   iconType = "Docker",
   title = "Docker Tutorial",
+  href = "https://youtu.be/pTFZFxd4hOI",
+  thumbnail = "/docker-image.jpeg",
+  alt = "Recomendation video",
 }: {
-  children: ReactElement;
   iconType: string;
   title: string;
+  href: string;
+  thumbnail: string;
+  alt?: string;
 }) {
   return (
-    <div className="relative box-border h-72 mr-10  rounded-sm  shadow-md">
-      {children}
+    <div className="relative box-border h-72 mr-10  rounded-sm  shadow-md overflow-hidden">
+      <a href={href} target="_blank">
+        <Image
+          src={thumbnail}
+          fill
+          alt={alt}
+          style={{ objectFit: "cover" }}
+          className="transform transition duration-500 hover:scale-110"
+        />
+      </a>
       <div className="absolute flex items-center justify-between p-5 w-full h-20 top-full -mt-20 left-0 backdrop-blur-md bg-white/30 z-10">
         <span className="text-white font-bold flex items-center">
           <Icons type={iconType} />
