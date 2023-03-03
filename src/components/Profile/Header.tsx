fd88e073aee5aeb1725ef3dd1e2bdcb35e248705
Jean Morillo Sanchez <jemorillosanchez@redventures.com>
2023-03-03 01:03:54 -0400

    Adds better formatting to header

--- src/components/Profile/Header.tsx
+++ src/components/Profile/Header.tsx
@@ -6,9 +6,9 @@ export default function ProfileHeader() {
   return (
     <header>
       <div
-        className={`${styles.fullWidth} relative rounded-lg animate-border inline-block bg-white from-blue-500 via-green-500 to-blue-100 bg-[length:400%_400%] p-0.5 bg-gradient-to-r`}
+        className={`${styles.fullWidth} relative rounded-b-lg animate-border inline-block bg-white from-blue-500 via-green-500 to-blue-100 bg-[length:400%_400%] p-0.5 bg-gradient-to-r`}
       >
-        <div className="block text-white bg-slate-900 w-full h-28 md:h-56 relative rounded-lg overflow-hidden">
+        <div className="block text-white bg-slate-900 w-full h-28 md:h-56 relative rounded-b-lg overflow-hidden">
           <Image
             className="relative object-cover transform transition duration-500 hover:scale-110 blur-sm"
             src={"/auroral-forest.jpeg"}
@@ -18,8 +18,8 @@ export default function ProfileHeader() {
           />
         </div>
       </div>
-      <div className="w-full flex flex-col xl:flex-row max-[377px]:-mt-7 -mt-16 md:pl-16 max-w-[1300px]">
-        <div className="flex flex-row  items-end md:items-center">
+      <div className="w-full flex flex-col xl:flex-row max-[377px]:-mt-7 -mt-16 md:-mt-28 md:pl-16 max-w-[1300px]">
+        <div className="flex flex-row items-end">
           <div className="max-[377px]:w-20 max-[377px]:h-20  w-32 h-32 md:w-56 md:h-56 relative rounded-full border-4 border-white shadow-md flex-shrink-0">
             <Image
               className="red object-cover w-5 rounded-full cursor-pointer"
@@ -36,19 +36,20 @@ export default function ProfileHeader() {
               className="absolute max-[377px]:left-12 max-[377px]:top-14 left-20 top-24 md:left-40 md:top-44 z-10 h-6 md:h-[30px]"
             />
           </div>
-          <div className="flex flex-col md:ml-6 items-start">
-            <span className="font-bold text-md md:text-[20px] text-gray-700">
+          <div className="flex flex-col ml-4 md:ml-6 items-start bottom-2 md:bottom-10 relative">
+            <span className="font-bold max-[377px]:text-sm text-md md:text-[20px] text-gray-700">
               Jean Morillo
             </span>
-            <span className="font-normal text-[12px] md:text-[14px] text-gray-500">
+            <span className="font-normal max-[377px]:text-[10px] text-[12px] md:text-[14px] text-gray-500">
               I&apos;m a Software Engineer in Puerto Rico
             </span>
           </div>
         </div>
-        <div className="flex items-center md:ml-auto relative pt-7 md:pt-0 pl-2 sm:pl-32 md:pl-0 ">
+        <div className="flex items-end md:ml-auto relative pt-7 md:pt-0 pl-2 sm:pl-36 md:pl-0 md:bottom-10 ">
           <a
             type="button"
             href="https://github.com/Morill30"
+            target="_blank"
             className="text-white bg-white border-[#4285F4] border-2 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-3 py-1.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#4285F4]/30 mr-2 mb-2"
           >
             <Icons type="Github" className="text-[#4285F4] w-6 h-6" />
