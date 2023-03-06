1322d6f566a72c0e351079839e60d03ea328cae6
Jean Morillo Sanchez <jemorillosanchez@redventures.com>
2023-03-06 00:41:00 -0400

    adds new components to header

--- src/components/Profile/Header.tsx
+++ src/components/Profile/Header.tsx
@@ -1,41 +1,30 @@
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
+import TopBackgroundHeader from "@/components/TopBackgroundHeader";
 import Icons from "@/styles/icons/Icons";
+import ImageModal from "@/components/Modals/ImageModal";
 
 export default function ProfileHeader() {
   return (
     <header>
-      <div
-        className={`${styles.fullWidth} relative rounded-b-lg animate-border inline-block bg-white from-blue-500 via-green-500 to-blue-100 bg-[length:400%_400%] p-0.5 bg-gradient-to-r`}
-      >
-        <div className="block text-white bg-slate-900 w-full h-28 md:h-56 relative rounded-b-lg overflow-hidden">
-          <Image
-            className="relative object-cover transform transition duration-500 hover:scale-110 blur-sm"
-            src={"/auroral-forest.jpeg"}
-            fill
-            style={{ objectFit: "cover" }}
-            alt="Background image"
-          />
-        </div>
-      </div>
+      <TopBackgroundHeader />
       <div className="w-full flex flex-col xl:flex-row max-[377px]:-mt-7 -mt-16 md:-mt-28 md:pl-16 max-w-[1300px]">
         <div className="flex flex-row items-end">
-          <div className="max-[377px]:w-20 max-[377px]:h-20  w-32 h-32 md:w-56 md:h-56 relative rounded-full border-4 border-white shadow-md flex-shrink-0">
-            <Image
-              className="red object-cover w-5 rounded-full cursor-pointer"
-              src={"/jean-avatar.jpeg"}
-              fill
-              style={{ objectFit: "cover" }}
-              alt="Background image"
-              onClick={() =>
-                window.alert("Will show a preview photo when clicked soon!")
-              }
-            />
-            <Icons
-              type="Verified-Checkmark"
-              className="absolute max-[377px]:left-12 max-[377px]:top-14 left-20 top-24 md:left-40 md:top-44 z-10 h-6 md:h-[30px]"
-            />
-          </div>
+          <ImageModal>
+            <div className="max-[377px]:w-20 max-[377px]:h-20  w-32 h-32 md:w-56 md:h-56 relative rounded-full border-4 border-white shadow-md flex-shrink-0">
+              <Image
+                className="red object-cover w-5 rounded-full cursor-pointer"
+                src={"/jean-avatar.jpeg"}
+                fill
+                style={{ objectFit: "cover" }}
+                alt="Background image"
+              />
+
+              <Icons
+                type="Verified-Checkmark"
+                className="absolute max-[377px]:left-12 max-[377px]:top-14 left-20 top-24 md:left-40 md:top-44 z-10 h-6 md:h-[30px]"
+              />
+            </div>
+          </ImageModal>
           <div className="flex flex-col ml-4 md:ml-6 items-start bottom-2 md:bottom-10 relative">
             <span className="font-bold max-[377px]:text-sm text-md md:text-[20px] text-gray-700">
               Jean Morillo
