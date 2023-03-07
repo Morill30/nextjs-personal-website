df8f7cc2eda725a3ae400931882122370091cd6a
Jean Morillo Sanchez <jemorillosanchez@redventures.com>
2023-03-06 22:27:56 -0400

    adds og image for whats app and other platforms

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,23 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Personal Website with experience and contact information for Jean Morillo"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          property="og:title"
+          content="Jean's personal site | Experience and more"
+        />
+        <meta
+          property="og:description"
+          content="Personal Website with experience and contact information for Jean Morillo"
+        />
+        <meta property="og:url" content="https://webjems.com" />
+        <meta
+          property="og:image"
+          content="https://webjems.com/profile-site.png"
+        />
+        <meta
+          property="twitter:image"
+          content="https://webjems.com/profile-site.png"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
